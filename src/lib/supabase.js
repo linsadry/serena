5e96c90e960da4faf6fcba332795eb5236218ae4
linsadry <adriana_lins@hotmail.com>
2026-05-27 21:31:29 -0300

    Update supabase.js

---
+++ src/lib/supabase.js
@@ -0,0 +1,2 @@
+const SUPABASE_URL = 'https://uxkjvbjlsbgmbalokisf.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV4a2p2Ympsc2JnbWJhbG9raXNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyMDEzOTAsImV4cCI6MjA5MTc3NzM5MH0.eOtAl-n3qNSLR0BQNKhr8jiE5qXResibjKVut0fpEHQ';
